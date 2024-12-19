dc75512bbdc9d19fd5403689ea6b5b3b06699a46
Cong Le <longchike@gmail.com>
2024-12-19 14:03:47 -0800

    initial setup

---
+++ Metal-Primitives/Metal_PrimitivesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metal_PrimitivesApp.swift
+//  Metal-Primitives
+//
+//  Created by Cong Le on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Metal_PrimitivesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
